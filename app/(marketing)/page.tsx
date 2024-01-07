b46cd2ad4dfcc2bc089aff1804fcff68f46cb71b
wlals7565 <117993640+wlals7565@users.noreply.github.com>
2024-01-07 16:10:31 +0900

    study

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { Medal } from "lucide-react";
+import Link from "next/link";
+import localFont from "next/font/local"
+
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2"
+})
+
+//TODO tailwind study
+//2. Taskify => threello
+const MarketingPage = () => {
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <div className="flex items-center justify-center flex-col">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+          <Medal className="h-6 w-6 mr-2" /> No 1 task management
+        </div>
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+          Takifgy helps team move
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 uppercase rounded-md">
+          work forward
+        </div>
+      </div>
+      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+        Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with taskify
+      </div>
+      <Button className="mt-6" size="lg" asChild>
+        <Link href="/sign-up">
+          Get Taskify for free
+        </Link>
+      </Button>
+    </div>
+  );
+};
+
+export default MarketingPage;
